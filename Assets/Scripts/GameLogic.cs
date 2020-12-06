920c3aa0e78398277193c19d0f2152cd0c5a80b6
FlyN Nick <nickassader@gmail.com>
2020-12-06 14:25:12 -0800

    Initial commit.
    Game logic already written, just need to come up with scenarios now.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,53 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField] Text textComponent = null;
+    [SerializeField] State startingState = null;
+
+    State state;
+
+	void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+	}
+	
+	void Update()
+    {
+        if (!DetectQuit()) { ManageState(); }
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        var stateChanged = false;
+        for (int index = 0; index < nextStates.Count; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+                stateChanged = true;
+            }
+        }
+        if (stateChanged)
+        {
+            textComponent.text = state.GetStateStory();
+        }
+    }
+
+    private bool DetectQuit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+            return true;
+        }
+        else { return false; }
+    }
+
+    private void Quit() { Application.Quit(); }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText = "";
+    [SerializeField] List<State> nextStates = new List<State>();
+    [SerializeField] bool quit = false;
+
+    public string GetStateStory()
+    {
+        if (quit) { Application.Quit(); }
+        return storyText;
+    }
+
+    public List<State> GetNextStates()
+    {
+        return nextStates;
+    }
+}
